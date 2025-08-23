dbcdb1768d5893f18d5b462f835bb2d95eea0195
Arseniy Nazarov <arsnazarov94@gmail.com>
2025-08-23 20:33:49 +0200

    Trim torrent links from header

--- sections/torrents/history.php
+++
@@ -1,9 +0,0 @@
-<?php
-/** @phpstan-var \Twig\Environment $Twig */
-
-$group = (new Gazelle\Manager\TGroup())->findById((int)($_GET['id'] ?? 0));
-if (is_null($group)) {
-    error(404);
-}
-
-echo $Twig->render('revision.twig', ['object' => $group]);
