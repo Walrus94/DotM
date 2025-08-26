e02a80ccba98a7742586d144564f161332a25b61
Arseniy Nazarov <arsnazarov94@gmail.com>
2025-08-26 10:51:08 +0200

    fix: drop bonus tables in dependency order

--- misc/phinx/migrations/20250101000001_drop_bonus_tables.php
+++ misc/phinx/migrations/20250101000001_drop_bonus_tables.php
@@ -5,7 +5,14 @@ use Phinx\Migration\AbstractMigration;
 
 final class DropBonusTables extends AbstractMigration {
     public function change(): void {
-        foreach (['bonus_history', 'bonus_item', 'bonus_pool', 'bonus_pool_contrib', 'user_bonus', 'contest_has_bonus_pool'] as $table) {
+        foreach ([
+            'bonus_history',
+            'bonus_pool_contrib',
+            'contest_has_bonus_pool',
+            'bonus_pool',
+            'bonus_item',
+            'user_bonus',
+        ] as $table) {
             if ($this->hasTable($table)) {
                 $this->table($table)->drop()->save();
             }
